04d02c3bf17631ba5d704b0f1bf2c8fce2ae8939
Heather Brysiewicz <h.a.brysiewicz@gmail.com>
2016-01-11 20:47:19 -0800

    Add browserify, babel, and sourcemaps

---
+++ app/test.js
@@ -0,0 +1,3 @@
+console.log('here')
+let b = [1,2,3]
+console.log(...b)
--- tasks/scripts.js
+++ tasks/scripts.js
@@ -1,11 +1,30 @@
 import gulp from 'gulp'
 import minify from 'gulp-minify'
-import rename from 'gulp-rename'
+import rename from 'vinyl-source-stream'
+import browserify from 'browserify'
+import glob from 'glob'
+import sourcemaps from 'gulp-sourcemaps'
+import buffer from 'vinyl-buffer'
+
+let handleErrors = (err) => {
+  console.error(err)
+  this.emit('end')
+}
 
 let scripts = () => {
-  return gulp.src('app/**/*.js')
-    .pipe(minify())
+  let filenames = glob.sync('app/**/*.js')
+  return browserify({
+      entries: filenames,
+      debug: true,
+      paths: ['./app']
+    })
+    .transform('babelify')
+    .bundle()
+    .on('error', handleErrors)
     .pipe(rename('index.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('public'))
 }
 
