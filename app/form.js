51ed390707ddc3a047e1d81ffb39239bb3008f0c
Stuart Runyan <shrunyan@gmail.com>
2016-01-28 23:18:36 -0800

    Add render logic

--- app/form.js
+++ app/form.js
@@ -5,8 +5,13 @@ var submitHandler = function(evt) {
   evt.preventDefault()
   var path = apiURL + '/forms'
   xhr('POST', path, serializeArray('form'), function(err, data) {
-    if (err) { throw err }
+    if (err) {
+      renderError(err)
+      throw err
+    }
     console.log(data)
+    renderFormData(data)
+    document.querySelector('form').reset()
   })
 }
 var serializeArray = function(selector) {
@@ -54,3 +59,59 @@ var xhr = function(method, path, data, callback) {
 }
 
 form.addEventListener('submit', submitHandler)
+
+
+var createElementWithTextNode = function(tagName, tagContent) {
+  var node = document.createElement(tagName)
+  if (tagContent) {
+    var textNode = document.createTextNode(tagContent)
+    node.appendChild(textNode)
+  }
+  return node
+}
+
+var renderError = function(error) {
+  var responseNode = document.querySelector('.response-wrapper')
+  var errorNode = createElementWithTextNode('div', error.toString())
+  errorNode.className = 'error'
+  responseNode.appendChild(errorNode)
+}
+
+var renderFormData = function(data) {
+  var responseNode = document.querySelector('.response-wrapper')
+
+  //generic success message
+  var successNode = createElementWithTextNode('div', 'You\'ve add a new card')
+  successNode.className = 'success'
+  responseNode.appendChild(successNode)
+
+  var dictionaryNode = document.createElement('dl')
+  var keys = ['name', 'email', 'github', 'twitter', 'city', 'state', 'bio']
+  keys.forEach(function(key) {
+    //create a dom node with the name of a value
+    var termNode = createElementWithTextNode('dt', key)
+    dictionaryNode.appendChild(termNode)
+
+    //create another dom node with the value
+    var definitionNode = createElementWithTextNode('dd', data[key])
+    dictionaryNode.appendChild(definitionNode)
+  })
+
+  var skillsTermNode = createElementWithTextNode('dt', 'skills')
+  dictionaryNode.appendChild(skillsTermNode)
+
+  var skillsDefinitionNode = document.createElement('dd')
+  var skillsList = document.createElement('ul')
+  skillsDefinitionNode.appendChild(skillsList)
+  dictionaryNode.appendChild(skillsDefinitionNode)
+
+  if (data.skills_attributes) {
+    data.skills_attributes.forEach(function (skill) {
+      var skillNode = createElementWithTextNode('li', skill.description)
+      skillsList.appendChild(skillNode)
+    })
+  }
+
+  dictionaryNode.className = 'response'
+  responseNode.appendChild(dictionaryNode)
+}
